6dd8111e22f283c3cac2fa1ccc621ceca0e8cb9e
ronald4992 <ronald.lopezc@uniagustiniana.edu.co>
2025-08-28 15:59:12 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,30 +5,27 @@ const urlsToCache = [
   './manifest.json'
 ];
 
-// Instalación del Service Worker y cacheo de recursos
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activación y limpieza de caches viejas
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys.filter(key => key !== CACHE_NAME)
             .map(key => caches.delete(key))
-      )
-    )
+      );
+    })
   );
 });
 
-// Responder con cache o con la red
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
 });
+

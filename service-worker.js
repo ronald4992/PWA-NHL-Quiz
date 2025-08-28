99f961fcbbfc87ca57eb736513f2f8f0bee9a1f2
ronald4992 <ronald.lopezc@uniagustiniana.edu.co>
2025-08-28 15:21:40 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,25 @@
-const CACHE_NAME = 'nhl-scores-cache-v1';
-const urlsToCache = [
-  '.', // cachea index.html
-  // No hay archivos externos, todo está inline salvo la imagen de logo que está en la web, pero podemos cachear esa también:
+const CACHE_NAME = 'nhl-pwa-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
   'https://cdn.worldvectorlogo.com/logos/nhl.svg'
 ];
 
-self.addEventListener('install', event => {
-  // Cacheamos los archivos durante la instalación
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
 
-self.addEventListener('fetch', event => {
-  // Respondemos con lo cacheado o con la petición en la red
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  // Limpiamos caches viejas si las hubiera
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
 });
